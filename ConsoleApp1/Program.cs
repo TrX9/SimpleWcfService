505aef457f31dee2348d9a23dfa86090497209f3
TrX9 <xxtmxx9@gmail.com>
2024-07-16 12:14:16 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+
+            var result = client.GetData(123);
+            Console.WriteLine(result);
+
+            var composite = new ServiceReference1.CompositeType
+            {
+                BoolValue = true,
+                StringValue = "Test"
+            };
+            var compositeResult = client.GetDataUsingDataContract(composite);
+            Console.WriteLine(compositeResult.StringValue);
+
+            client.Close();
+        }
+    }
+}
